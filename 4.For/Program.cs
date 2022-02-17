a1b5272b4b4c73aab4ab526591ef4d907c1cc0ff
aidancer55 <bluebonnets77@gmail.com>
2022-02-17 21:10:45 +0300

    Добавьте файлы проекта.

---
+++ 4.For/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int n = 1;
+            int a = 0;
+            for (; n <= N; n++)
+            {
+                a = a + 2 * n - 1;
+                Console.WriteLine("Квадрат числа {0} равен {1}", n, a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
