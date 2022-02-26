fff7c7ff83f55c61bc0f156727c3bbecc3f867a5
aidancer55 <bluebonnets77@gmail.com>
2022-02-26 13:20:54 +0300

    1

---
+++ Ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int N;
+            do
+            {
+                N = Convert.ToInt32(Console.ReadLine());
+                if (N > 0)
+                {
+                    a++;
+                }
+                else if (N < 0)
+                {
+                    b++;
+                }
+            } while (N > 0 || N < 0);
+
+
+            if (a > b)
+            {
+                Console.WriteLine("Положительных чисел больше");
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("Отрицательных чисел больше");
+            }
+            else
+            {
+                Console.WriteLine("Число 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = 0;
+            while (A >= C)
+            {
+                a++;
+                A = A - C;
+            }
+            while (B >= C)
+            {
+                b++;
+                B = B - C;
+            }
+
+            Console.WriteLine("Количество квадратов {0}", a * b);
+            Console.ReadKey();
+        }
+    }
+}
